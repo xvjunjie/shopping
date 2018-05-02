4c871ec38b2c23f8fd0de0bad80dc04bb6acb0c1
xvjunjie <1559643591@qq.com>
2018-05-02 18:07:49 +0800

    drf  ModelSerializer

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,7 +1,21 @@
 # -*- coding:utf-8 -*-
 from rest_framework import serializers
 
+from goods.models import Goods, GoodsCategory
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, max_length=100)
-    click_num = serializers.IntegerField(default=0)
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    ''' Serializer'''
+    # Serializer
+    # name = serializers.CharField(required=True, max_length=100)
+    # click_num = serializers.IntegerField(default=0)
+    category = CategorySerializer()
+    class Meta:
+        model = Goods
+        fields = '__all__'
