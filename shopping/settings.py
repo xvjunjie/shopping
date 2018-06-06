b62f0657b9b92d1ba955ea78a1fd107fe893891c
xvjunjie <1559643591@qq.com>
2018-06-06 22:08:53 +0800

    view的继承方式跟分页

--- shopping/settings.py
+++ shopping/settings.py
@@ -49,18 +49,24 @@ INSTALLED_APPS = [
     'xadmin',
     'rest_framework',
     'django_filters',
+    'corsheaders',
+    'rest_framework.authtoken',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'shopping.urls'
 
@@ -144,7 +150,15 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
+    # 分页
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
+    # 过滤器
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    # 用户授权
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
--- shopping/urls.py
+++ shopping/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.views.static import serve
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 import xadmin
 from shopping.settings import MEDIA_ROOT
@@ -36,5 +37,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^docs/', include_docs_urls(title='title')),
     # 登陆的一个配置
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', views.obtain_auth_token)
 ]
