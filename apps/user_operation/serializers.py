bf701782867d578845d5a032dcca1dbbeb30e0ff
xvjunjie <1559643591@qq.com>
2018-06-08 16:22:35 +0800

    收货地址

--- apps/user_operation/serializers.py
+++ apps/user_operation/serializers.py
@@ -1,12 +1,10 @@
 # -*- coding:utf-8 -*-
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from .models import UserLeavingMessage
+from .models import UserLeavingMessage, UserAddress
 
 
 class LeavingMessageSerializer(ModelSerializer):
-
-
     user = serializers.HiddenField(
         default=serializers.CurrentUserDefault()
     )
@@ -15,3 +13,14 @@ class LeavingMessageSerializer(ModelSerializer):
     class Meta:
         model = UserLeavingMessage
         fields = ("user", "message_type", "subject", "message", "file", "id", "add_time")
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+    add_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M')
+
+    class Meta:
+        model = UserAddress
+        fields = ("id", "user", "province", "city", "district", "address", "signer_name", "add_time", "signer_mobile")
--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from .models import UserLeavingMessage
-from .serializers import LeavingMessageSerializer
+from .models import UserLeavingMessage, UserAddress
+from .serializers import LeavingMessageSerializer, AddressSerializer
 
 
 # Create your views here.
@@ -28,3 +29,24 @@ class LeavingMessageViewset(CreateModelMixin, DestroyModelMixin, ListModelMixin,
 
     def get_queryset(self):
         return UserLeavingMessage.objects.filter(user=self.request.user)
+
+
+
+class AddressViewset(ModelViewSet):
+    """
+    收货地址管理
+    list:
+        获取收货地址
+    create:
+        添加收货地址
+    update:
+        更新收货地址
+    delete:
+        删除收货地址
+    """
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    serializer_class = AddressSerializer
+
+    def get_queryset(self):
+        return UserAddress.objects.filter(user=self.request.user)
